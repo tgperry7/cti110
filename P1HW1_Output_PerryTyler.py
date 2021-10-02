650bfe48ea0b805b0991a61a9f13db82b03a5ba7
tgperry7 <91351995+tgperry7@users.noreply.github.com>
2021-10-02 16:01:12 -0400

    Add files via upload

---
+++ P1HW1_Output_PerryTyler.py
@@ -0,0 +1,10 @@
+print("Enter integer:")
+user_num = int(input())
+print("You entered:",user_num)
+print(user_num,"squared is", user_num * user_num)
+print("And",user_num,"cubed is", user_num * user_num * user_num, "!!")
+
+print("Enter another integer:")
+user_num2 = int(input())
+print(user_num,'+',user_num2,"is", user_num + user_num2)
+print(user_num,'*',user_num2,"is", user_num * user_num2)
