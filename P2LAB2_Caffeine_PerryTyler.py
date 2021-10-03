1e5196bc805097abe805a2055075d537a57abf26
tgperry7 <91351995+tgperry7@users.noreply.github.com>
2021-10-03 14:37:02 -0400

    Add files via upload

---
+++ P2LAB2_Caffeine_PerryTyler.py
@@ -0,0 +1,4 @@
+caffeine_mg = float(input())
+print("After 6 hours: {:.2f} mg".format(caffeine_mg / 2))
+print("After 12 hours: {:.2f} mg".format(caffeine_mg / 4))
+print("After 24 hours: {:.2f} mg".format(caffeine_mg / 16))
